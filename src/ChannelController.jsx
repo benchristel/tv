ca44dc9ceecaf755b6ee8ecc8e34a7c1fe13a998
Ben Christel <ben.christel@gmail.com>
2022-08-23 17:50:30 -0700

    Extract channels

--- src/ChannelController.jsx
+++ src/ChannelController.jsx
@@ -8,6 +8,7 @@ import { createChannel } from "./Channel"
 import { channel1Videos, channel2Videos } from "./data/shows"
 import { albums } from "./data/music"
 import type { Episode, Video } from "./data/types"
+import { channels } from "./data/channels"
 
 type Props = {|
   onChange: () => mixed,
@@ -33,15 +34,4 @@ export function ChannelController(props: Props): React.Node {
       </div>
     </>
   )
-}
-
-const channels = [
-  // PROTOTYPE: for now, each episode just has one video in it.
-  createChannel("Channel 1", channel1Videos.map(singleVideoEpisode)),
-  createChannel("Channel 2", channel2Videos.map(singleVideoEpisode)),
-  createChannel("Channel 3", albums),
-]
-
-function singleVideoEpisode(v: Video): Episode {
-  return { videos: [v] }
 }
---
+++ src/data/channels.js
@@ -0,0 +1,19 @@
+// @flow
+import type { Channel } from "../Channel"
+
+import type { Episode, Video } from "./types"
+
+import { createChannel } from "../Channel"
+import { albums } from "./music"
+import { channel1Videos, channel2Videos } from "./shows"
+
+export const channels: Array<Channel> = [
+  // PROTOTYPE: for now, each episode just has one video in it.
+  createChannel("Channel 1", channel1Videos.map(singleVideoEpisode)),
+  createChannel("Channel 2", channel2Videos.map(singleVideoEpisode)),
+  createChannel("Channel 3", albums),
+]
+
+function singleVideoEpisode(v: Video): Episode {
+  return { videos: [v] }
+}
