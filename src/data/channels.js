9d8992d2a3069fe27eaa873d491671524030da74
Ben Christel <ben.christel@gmail.com>
2022-08-28 20:43:51 -0700

    Delete outdated comment

--- src/data/channels.js
+++ src/data/channels.js
@@ -8,7 +8,6 @@ import { albums } from "./music"
 import { channel1Videos, channel2Videos, debuggingVideos } from "./shows"
 
 export const channels: Array<Channel> = [
-  // PROTOTYPE: for now, each episode just has one video in it.
   createChannel("Channel 1", channel1Videos.map(singleVideoEpisode)),
   createChannel("Channel 2", channel2Videos.map(singleVideoEpisode)),
   createChannel("Channel 3", albums),
