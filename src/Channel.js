e8d649fd59e6e70a5c87f88e258480deab22e322
Ben Christel <ben.christel@gmail.com>
2022-09-04 13:16:33 -0700

    Test schedule generation

--- src/Channel.js
+++ src/Channel.js
@@ -7,6 +7,7 @@ import { mulberry32 } from "./lib/random"
 
 import type { Broadcast } from "./Broadcast"
 import type { Episode } from "./data/types"
+import { equals, expect, test } from "@benchristel/taste"
 
 export interface Channel {
   getBroadcast(time: number): Broadcast;
@@ -26,34 +27,7 @@ type Schedule = Array<
 export const GAP_SECONDS = 2
 
 export function createChannel(name: string, episodes: Array<Episode>): Channel {
-  const getSchedule = cache(1, (seed) => {
-    console.debug("seed", seed)
-    const rng = mulberry32(cyrb128_32(seed))
-    let totalDuration = 0
-    let schedule: Schedule = []
-    while (totalDuration < 24 * 3600) {
-      const episode = pick(episodes, rng())
-
-      for (const video of episode.videos) {
-        schedule.push(
-          {
-            type: "nothing",
-            durationSeconds: GAP_SECONDS,
-            nextVideoId: video.videoId,
-          },
-          {
-            type: "video",
-            videoId: video.videoId,
-            videoTitle: video.title,
-            durationSeconds: video.durationSeconds,
-          }
-        )
-        totalDuration += video.durationSeconds + GAP_SECONDS
-      }
-    }
-    console.debug("channel " + name, schedule)
-    return schedule
-  })
+  const getSchedule = cache(1, ScheduleGenerator(episodes))
 
   return {
     getBroadcast,
@@ -92,6 +66,145 @@ export function createChannel(name: string, episodes: Array<Episode>): Channel {
   }
 }
 
+const ScheduleGenerator = (episodes: Array<Episode>) => (seed: string) => {
+  const rng = mulberry32(cyrb128_32(seed))
+  let totalDuration = 0
+  let schedule: Schedule = []
+  while (totalDuration < 24 * 3600) {
+    const episode = pick(episodes, rng())
+
+    for (const video of episode.videos) {
+      schedule.push(
+        {
+          type: "nothing",
+          durationSeconds: GAP_SECONDS,
+          nextVideoId: video.videoId,
+        },
+        {
+          type: "video",
+          videoId: video.videoId,
+          videoTitle: video.title,
+          durationSeconds: video.durationSeconds,
+        }
+      )
+      totalDuration += video.durationSeconds + GAP_SECONDS
+    }
+  }
+  return schedule
+}
+
 function add(a, b) {
   return a + b
 }
+
+// TESTS =====================================================================
+
+test("ScheduleGenerator", {
+  "schedules a single 24-hour video"() {
+    const episodes = [
+      {
+        videos: [
+          {
+            durationSeconds: 3600 * 24,
+            videoId: "the-video-id",
+            title: "the-title",
+          },
+        ],
+      },
+    ]
+    const generator = ScheduleGenerator(episodes)
+    expect(generator(""), equals, [
+      { type: "nothing", durationSeconds: 2, nextVideoId: "the-video-id" },
+      {
+        type: "video",
+        videoId: "the-video-id",
+        videoTitle: "the-title",
+        durationSeconds: 86400,
+      },
+    ])
+  },
+
+  "schedules a 12-hour video twice"() {
+    const episodes = [
+      {
+        videos: [
+          {
+            durationSeconds: 3600 * 12,
+            videoId: "the-video-id",
+            title: "the-title",
+          },
+        ],
+      },
+    ]
+    const generator = ScheduleGenerator(episodes)
+    expect(generator(""), equals, [
+      { type: "nothing", durationSeconds: 2, nextVideoId: "the-video-id" },
+      {
+        type: "video",
+        videoId: "the-video-id",
+        videoTitle: "the-title",
+        durationSeconds: 43200,
+      },
+      { type: "nothing", durationSeconds: 2, nextVideoId: "the-video-id" },
+      {
+        type: "video",
+        videoId: "the-video-id",
+        videoTitle: "the-title",
+        durationSeconds: 43200,
+      },
+    ])
+  },
+
+  "picks the videos to show randomly"() {
+    const episodes = [
+      {
+        videos: [
+          {
+            durationSeconds: 3600 * 2,
+            videoId: "one",
+            title: "",
+          },
+        ],
+      },
+      {
+        videos: [
+          {
+            durationSeconds: 3600 * 2,
+            videoId: "two",
+            title: "",
+          },
+        ],
+      },
+      {
+        videos: [
+          {
+            durationSeconds: 3600 * 2,
+            videoId: "three",
+            title: "",
+          },
+        ],
+      },
+    ]
+    const generator = ScheduleGenerator(episodes)
+    expect(
+      generator("asdfx")
+        .map((v) => v.videoId)
+        .filter(Boolean),
+      equals,
+      [
+        "three",
+        "one",
+        "three",
+        "two",
+        "two",
+        "three",
+        "one",
+        "three",
+        "two",
+        "two",
+        "one",
+        "one",
+      ]
+    )
+  },
+})
