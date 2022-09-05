dafe8b194c15714aab711156669ac8b11615065b
Ben Christel <ben.christel@gmail.com>
2022-09-05 12:44:01 -0700

    yarn build

--- docs/src/data/scrape.js
+++ docs/src/data/scrape.js
@@ -1,7 +1,7 @@
 console.log(/\/watch\?/.test(window.location) ? [
   /v=(.{11})/.exec(window.location)?.[1],
   document.querySelector(".ytp-time-duration").innerText.trim(),
-  document.querySelector(".ytd-video-primary-info-renderer").innerText.split("\n")[0].trim()
+  document.querySelector(".title .ytd-video-primary-info-renderer").innerText.split("\n")[0].trim()
 ].join(" ") : /\/playlist\?/.test(window.location) ? [...document.querySelectorAll("ytd-playlist-video-renderer")].map((v) => {
   const link = v.querySelector("a#video-title")?.href ?? "";
   const id = /v=(.{11})/.exec(link)?.[1];
