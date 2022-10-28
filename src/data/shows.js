435818f3da0e7697bc2959819443b37cf7ba8657
Gavin Morgan <morgan.gavin@gmail.com>
2022-10-28 14:03:35 -0700

    anime mecha opening credits

--- src/data/shows.js
+++ src/data/shows.js
@@ -1300,4 +1300,106 @@ export const mechaThemeSongs: Array<Video> = parseVideos(`
   Pf10yCUCq88 3:23 Blood Reign: Curse of the Yoma OST - Midori Karashima - Ashita e no Prologue (Prologue for Tomorrow)
   iFtKaw3Jksc 3:36 Dangaioh full opening song
   jCF3Z3L5SuM 4:12 Project A-ko 3: Cinderella Rhapsody - ED [Get a Chance!]
+
+  # Mecha Anime Openings https://www.youtube.com/playlist?list=PLZ6GAQ4derFa_0gbIAJ_SLK-kOBDVWUPG
+  c2zYuQqzEJQ 1:15 1963 Tetsujin 28 gou Opening Tetsujin 28 gou Duke Aces] [CKR] [0AA53060]
+  2m1m-YWgQ84 1:11 Astroganger 1972 Opening
+  MR7xA2JbNCQ 1:02 Mazinger Z Opening 1 Japones | マジンガーZ
+  W6ahWjlP3q4 1:09 Getter Robo 1974 Opening
+  UDjkXkMTg-c 1:01 Great Mazinger Opening
+  07iM9PT-buE 1:10 Yuusha Raideen OP (English Captions)
+  jjieygKSXpQ 1:10 Getter Robo G Opening
+  9n3GEsKwhis 1:28 1975 Uchuu no Kishi Tekkaman Opening Tekkaman no Uta Mizuki Ichirou] [CKR] [53A71406]
+  tW8JKCouIA4 1:12 Grendizer Opening
+  pA0Uj3BJ1d0 2:01 Jeeg - opening (original version)
+  G4pNfyHIi2Y 1:11 Gaiking Japanese Opening HQ
+  Z52A2Nb6Q24 1:17 Dai Apolón Intro Japonés
+  FNiDo84BRFE 1:15 초전자로보 콤바트라 V 오프닝 combattler v Opening
+  _Qr0vzhMVT0 1:02 Groizer X
+  xuKt5BUy7nA 1:01 Gakeen Opening
+  rZam2ZC81bw 1:25 Mekanda Robo
+  6tiUoS9-tCI 1:11 Planet Robo Danguard Ace Opening
+  3wpipSyYsTE 1:09 VOLTES V
+  Lt-aOnmKQhE 2:00 Balatack
+  32JJ7O65nm0 1:32 Invincible Superman Zambot 3 Opening
+  zQgVxUCO4RA 1:07 General Daimos Opening
+  edMaT41DwOQ 1:32 無敵鋼人ダイターン3 OP (STEREO)
+  L80qGAVEOTQ 2:54 宇宙魔神ダイケンゴー(ナレーション入りFULL)
+  WT8itiVr4ng 2:20 未来ロボ ダルタニアス OP・ED
+  GLWLy_Cz2ZY 2:46 Sub Latino, GUNDAM Opening, Vuela Gundam! 飛べ！ガンダム　機動戦士 ガンダム Hi Res Audio.
+  k7dm5Gx8xUo 1:20 Gordian intro
+  soMcPAwIsgk 1:27 Trider G7 Opening JAPANESE
+  kpojMJXEe1M 1:20 God Sigma original opening title
+  TIcYvE231q8 1:21 Space Runaway Ideon Opening 【HD】
+  N2NX6cVMlkM 1:24 Space_Warrior_Baldios OP HD
+  4hSuPdW_i_s 1:31 SAIKYO ROBO DAIOJA OP
+  eBekEB-bMdQ 1:17 gold lighter op
+  bVw5khpgbSw 1:16 Golion OP
+  1f-oASal95Y 1:31 Goshogun - opening (original version)
+  jLFcGXmJ6MM 3:27 六神合体ゴッドマーズ OP (FULL) GOD MARS
+  R9OolcoJKpk 1:21 銀河旋風ブライガー Galaxy Cyclone Braiger OP (Eng Subs)
+  IE6IKESS45E 1:32 1981 Taiyou no Kiba Dagram Opening Saraba Yasashiki Hibi yo Asada Mamoru] [CKR] [53CA8B8A]
+  tpoIiK8gk8c 1:32 xabungle OP
+  zbUBeU81s_s 1:32 魔境伝説アクロバンチ OP「夢の狩人」
+  a73e3LjFwUo 1:30 銀河烈風バクシンガー OP「銀河烈風バクシンガー」
+  sAUwdwGOh7E 1:30 Macross Op
+  qK_pDGzJubo 1:21 亜空大作戦スラングル 前期OP「亜空大作戦のテーマ」
+  fn1JAY6M6jU 1:32 聖戦士ダンバイン/ Aura Battler Dunbine OP BD (1080P HD)
+  j8t4cHz9IAQ 1:18 Lightspeed Electroid Albegas intro
+  J8hwDQJRU7E 1:31 Armored Trooper Votoms OP
+  GgP4aMcm5dU 1:32 銀河疾風サスライガー OP「銀河疾風サスライガー」
+  ztw3EaoxPJM 4:43 Sky-hurricane (Augus-opening Theme)
+  yBR8PRoUP28 1:31 Psycho Armor Govarian OP [English subtitles]
+  SehHbl24gQk 1:23 Genesis Climber Mospeada Opening HD
+  fDIXVIWc0CQ 1:15 Opening Dorvack
+  Qwszwb3aIPg 1:31 VIFAM OP
+  Nuvca6MPO0s 1:31 L-GAIM OP 1
+  kK8uSufYqD0 3:15 Video Warrior Laserion Full Theme (1984)
+  N7st-s83IFk 1:31 Giant Gorg Creditless OP [English Captions]
+  f7UUQd7lLbA 2:00 Super Dimension Southern Cross OP
+  00E3arZ_aiU 4:15 God Mazinger - DREAMY MY LOVE (MIQ version)
+  s2CiX6ArmsQ 1:31 Panzer World Galient OP
+  UIyKptTyuPw 1:29 超力ロボ・ガラット OP [STEREO]
+  WA8OYMwqkM8 1:45 Sei Jushi Bismark Opening
+  vbAk5UcZGY0 2:18 Megazone 23 Part 1 Opening
+  I5cRFv5iLvw 1:22 Dancougar - Opening [HD Remastered]
+  gJWpOqrMtjs 1:42 Aoki Ryuusei SPT Layzner OP HD (Textless)
+  Uq_TD0_2_1s 1:39 ninja sensi tobikage op
+  t02JzTMJxKQ 3:23 Fight! Iczer One - Eternal Iczer-1
+  9IBkG8E4qYc 1:32 Gundam ZZ opening HD
+  xl4IqLfzHwg 1:31 Machine Robo: Revenge of Cronos OP
+  eSMAsoLvEXw 1:40 머신로보 배틀 해커즈 (マシンロボ ぶっちぎりバトルハッカーズ) (Machine Robo Battle Hackers) OP
+  ItCMX3A1qaE 1:31 DRAGONAR OP 1
+  oQKavTdZKvA 1:21 Transformers The Headmasters Opening (HD)
+  8BcM31lohl0 1:30 Dangaioh OP
+  w0rb3VaSt7E 2:29 Guardian Deity - (Aura Battler Dunbine Palladium OST)
+  xE6SAE1tCEE 5:35 Beyond The Time
+  gX_8tobdiMc 1:22 Transformers Master Force Opening
+  ZQxC1vGgyoo 1:30 Eiyuuden Mashin Wataru opening
+  fMpI_bc3LrI 1:31 Mobile Police Patlabor 1988 (Early Days OVA) - opening [HD] Remastered
+  GMISeKT5c-U 1:25 Opening Gunbuster Japones
+  KkF13YQkPx0 3:56 Awake! Zeorymer(High Quality Sound)
+  wo8W-kuPuxg 1:32 Bio Armor Jushin Liger Opening 1
+  1Khksntkwis 1:21 Transformers Victory OP subtitulado
+  1WwZPwVLR08 3:53 いつか空に届いて
+  dUSNV-A8gts 1:30 GRANZORT
+  Adno8sRTono 3:55 Mobile Police PATLABOR the Movie - Ending Theme
+  Z9UtUzjUA5Q 1:32 Patlabor on Television - Opening [HD] Remastered
+  GkFoLL0QmFQ 1:30 Exkaiser Op English Sub
+  SNGoAkdr6hU 1:32 Mashin Hero Wataru 2 Opening 2 "Fight!"
+  OxGilsRr5Yk 1:27 NG Knight Ramune & 40 - clean OP1
+  4Z4l0vKVnSA 1:21 Opening Titles : Transformers: Zone
+  k3WMr1EhRL8 1:34 Patlabor: The New Files OP2 (mamoritaino)HQ
+  DA9asP4wCi0 1:31 太陽の勇者ファイバード OP
+  rgfIFp0lfog 1:20 Getter Robo Go Opening
+  uM3Ltvq22fk 4:39 Kimi wo mitumete
+  cD28CY-ZAl8 1:30 raijin oh opening
+  eP0U3irjIow 4:10 THE WINNER
+  8mDvhVVAqf0 1:22 Detonator ORGUN
+  NdpAOPKSMSo 1:31 ダ・ガーン OP 2
+  pmX564UTabE 1:32 Tekkaman Blade 1st opening
+  rHbWxMryIls 1:32 Genki Bakuhatsu Ganbarugar Creditless OP
+  1lG1BToaLTw 1:38 鉄人２８号ＦＸ Tetsujin 28-Gou FX Opening
+  o3Q0aNdLtds 1:52 Giant Robo The Animation (1992) Intro scene 1080p
+  g_AZikitKcs 1:31 Might Gaine textless OP1
 `)
