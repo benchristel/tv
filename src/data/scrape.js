68a4add8f78c381749a02143b275591abffa263d
Ben Christel <ben.christel@gmail.com>
2022-12-13 21:25:57 -0800

    Make scraper script bookmarkletable

--- src/data/scrape.js
+++ src/data/scrape.js
@@ -1,44 +1,43 @@
-// SCRAPING A CHANNEL, VIDEO, OR PLAYLIST PAGE
-// Paste this into the JS console on YouTube.
-// Right-click the text that gets printed and select "Copy Object".
-console.log(
-  // Video page
+function last(a) {return a[a.length - 1]}
+videos =
   /\/watch\?/.test(window.location)
     ? [
         /v=(.{11})/.exec(window.location)?.[1],
-        document.querySelector(".ytp-time-duration").innerText.trim(),
+        last(document.querySelectorAll(".ytp-time-duration")).innerText.trim(),
         document
           .querySelector(".title .ytd-video-primary-info-renderer")
           .innerText.split("\n")[0]
           .trim(),
       ].join(" ")
-    : // Playlist
+    :
     /\/playlist\?/.test(window.location)
     ? [...document.querySelectorAll("ytd-playlist-video-renderer")]
         .map((v) => {
-          const link = v.querySelector("a#video-title")?.href ?? ""
-          const id = /v=(.{11})/.exec(link)?.[1]
+          const link = v.querySelector("a#video-title")?.href ?? "";
+          const id = /v=(.{11})/.exec(link)?.[1];
           const time = v
             .querySelector(
               "span#text.ytd-thumbnail-overlay-time-status-renderer"
             )
-            ?.innerText.trim()
-          const title = v.querySelector("#video-title")?.innerText
+            ?.innerText.trim();
+          const title = v.querySelector("#video-title")?.innerText;
           return `${id} ${time} ${title}`
         })
         .join("\n")
-    : // Channel page
+    :
       [...document.querySelectorAll("ytd-grid-video-renderer")]
         .map((v) => {
-          const link = v.querySelector("#thumbnail")?.href ?? ""
-          const id = /v=(.{11})/.exec(link)?.[1]
+          const link = v.querySelector("#thumbnail")?.href ?? "";
+          const id = /v=(.{11})/.exec(link)?.[1];
           const time = v
             .querySelector(
               "span#text.ytd-thumbnail-overlay-time-status-renderer"
             )
-            ?.innerText.trim()
-          const title = v.querySelector("#video-title")?.innerText
-          return `${id} ${time} ${title}`
+            ?.innerText.trim();
+          const title = v.querySelector("#video-title")?.innerText;
+          return `${id} ${time} ${title}`;
         })
-        .join("\n")
-)
+        .join("\n");
+
+console.log(videos);
+navigator.clipboard.writeText(videos).catch((e) => console.error(e));
