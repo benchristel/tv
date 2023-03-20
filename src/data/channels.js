aaceebb584029c7e4f7feb1ac0928c95901b4e7c
Ben Christel <ben.christel@gmail.com>
2023-03-19 20:37:35 -0700

    Add Finnish LOTR audiobook

--- src/data/channels.js
+++ src/data/channels.js
@@ -9,7 +9,6 @@ import {
   channel1Episodes,
   channel1Videos,
   channel2Videos,
-  channel2Episodes,
   channel4Videos,
   debuggingVideos,
 } from "./shows"
@@ -20,10 +19,7 @@ export const channels: Array<Channel> = [
     ...channel1Episodes,
     ...channel1Videos.map(singleVideoEpisode),
   ]),
-  createChannel("Channel 2", [
-    ...channel2Episodes,
-    ...channel2Videos.map(singleVideoEpisode),
-  ]),
+  createChannel("Channel 2", channel2Videos.map(singleVideoEpisode)),
   createChannel("Channel 3", albums),
   createChannel("Channel 4", channel4Videos.map(singleVideoEpisode)),
   createChannel("🎄", [
