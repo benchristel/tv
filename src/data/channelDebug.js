02df00babdefca39b8efbc8d82f3eb0935027e84
Ben Christel <ben.christel@gmail.com>
2024-07-21 21:24:54 -0700

    Put Muppet Weird Stuff on the debug channel

--- src/data/channelDebug.js
+++ src/data/channelDebug.js
@@ -1,12 +1,63 @@
 // @flow
 
-export const episodes: Array<string> = []
+export const episodes: Array<string> = [`
+  Z890lqjBkq4 0:00-1:02
+  ATorS9krpDU 0:26-2:31 The Muppet Show - 108: Paul Williams - “All Of Me” (1976)
+  Z890lqjBkq4 1:06-1:20
+  tYeqksmmCAY 0:13-1:14 The Muppet Show - 223: John Cleese - Gonzo’s Stunt: Cannonball (1978)
+  Gjz8oE4c8VQ 2:39 The Muppet Show - 223: John Cleese - Backstage #3 (1978)
+  Z890lqjBkq4 1:23-1:49
+  WktUcZVPGho 2:53 The Muppet Show - 118: Phyllis Diller - “Hugga Wugga”/“You Are My Sunshine” (1976)
+  Z890lqjBkq4 1:51-2:06
+  0nCfIA_Yy5U 0:50 The Muppet Show - 118: Phyllis Diller - Muppet Labs: Exploding Clothes (1976)
+  Z890lqjBkq4 2:08-2:14
+  q4BhmKStq1c 1:45 The Muppet Show - 205: Judy Collins - Planet Koozebane: Koozebanian Phoob (1978)
+  Z890lqjBkq4 2:17-2:30
+  mIJdUHY_tlo 0:20-1:08 The Muppet Show - 204: Rich Little - “Tea for Two” (1978)
+  Z890lqjBkq4 2:34-2:52
+  wHW-mOvh-Z0 1:16 The Muppet Show - 112: Peter Ustinov - “You Do Something To Me” (1976)
+  Z890lqjBkq4 2:54-2:59
+  H3Cq4Qk_mS8 0:51 The Muppet Show - 207: Edgar Bergen - Gonzo’s Stunt: Brick (1978)
+  Z890lqjBkq4 3:02-3:23
+  ZNdY-cm9l2Y 1:54 The Muppet Show - 206: Nancy Walker - A man listens to the singing of several birds (1978)
+  Z890lqjBkq4 3:25-3:38
+  vi8S0Fab2BI 0:14-1:53 The Muppet Show - 306: Jean Stapleton - “I’m Just Wild About Harry” (1978)
+  Z890lqjBkq4 3:42-4:11
+  ZJKF1f9TMO4 2:28 The Muppet Show - 506: Brooke Shields - “Jabberwocky” (1980)
+  Yo2gdnPU2yU 2:01 The Muppet Show - 208: Steve Martin - UK Spot: Gonzo and Yolanda (1977)
+  Z890lqjBkq4 4:13-4:33
+  GEp_7UetpUE 0:45-2:03 The Muppet Show - 109: Charles Aznavour - “I Feel Pretty” (1976)
+  Z890lqjBkq4 4:37-4:51
+  H3Yxyjo8WiU 2:01 The Muppet Show - 217: Julie Andrews - At The Dance/“Won’t Somebody Dance With Me” (1978)
+  Z890lqjBkq4 4:55-5:04
+  wz4KphWTVUE 0:15-3:30 The Muppet Show - 211: Dom DeLuise - Planet Koosebane: Merdildops (1977)
+  ZeBdhrNvZKM 0:41-3:02 The Muppet Show - 119: Vincent Price - “I’ve Got You Under My Skin” (1977)
+  Z890lqjBkq4 5:14-5:36
+  wE6sRJHGY6A 2:26 The Muppet Show - 324: Cheryl Ladd - Gonzo’s Stunt: Hypnotizing (1979)
+  Z890lqjBkq4 5:39-5:47
+  U67_apsJvvg 0:10-2:43 The Muppet Show - 119: Vincent Price - “You’ve Got a Friend” (1977)
+  Z890lqjBkq4 5:50-5:58
+  GNhx2udfVNA 0:11-0:50 The Muppet Show - 101: Juliet Prowse - Gonzo’s Stunt: Eating a Rubber Tire (1976)
+  4JE6YEjbMkc 0:04-0:41 Gonzo stops taxi - The Great Muppet Caper
+  S3LFe3N8VGc 1:01 The Muppet Show - 301: Kris Kristofferson & Rita Coolidge - Gonzo’s Stunt: Piano (1978)
+  YkuC2-HP8EY 1:14 The Muppet Show - 504: Shirley Bassey - “Liebesträume” (1980)
+  JG0nkfcuULM 0:43 The Muppet Show - 222: Teresa Brewer - Gonzo’s Stunt: Shakespeare (1978)
+  ElKK24cVzLk 1:37 The Muppet Show - 410: Kenny Rogers - Gonzo’s Stunts: Catapult (1979)
+  bGVxW8J_LjI 1:07 The Muppet Show - 217: Julie Andrews - Gonzo’s Stunt: “Eine Kleine Nachtmusik”(1978)
+  xkdVWkAZryc 1:01 The Muppet Show - 217: Julie Andrews - Backstage #2 (1978)
+  x27pFBKEYoI 0:17-1:13 The muppets take manhattan
+  Z890lqjBkq4 6:45-6:58
+  iVJhbjJ9Ke4 2:51 The Muppet Show - 209: Madeline Kahn - Backstage #4/“Wishing Song” (1977)
+  Z890lqjBkq4 7:01-7:14
+  jJDFV9o-HKc 1:18 The Muppet Show - 504: Shirley Bassey - “Isn’t This a Lovely Day” (1980)
+  Z890lqjBkq4 7:17-7:50
+  Vo3LHOM5Ed8 1:27 The Muppet Show - 217: Julie Andrews - Talk Spot (1978)
+  Z890lqjBkq4 7:54-8:00
+  IyG3PL5V490 2:37 The Muppet Show - 107: Florence Henderson - Planet Koozebane (1976)
+  Z890lqjBkq4 8:03-8:15
+  gjsetOXNQHQ 2:06 The Muppet Show - 409: Beverly Sills - UK Spot: “Muppera” (1979)
+  Z890lqjBkq4 8:19-9:11
+`]
 
 export const videos: string = `
-#vKXu0CzRcrI 0:16 Here's Tree
-#ieWm9T_GgSA 0:08 I'm Lost
-zBIx-gP-I8c 0-20,40-51 Strong Bad Email #1 - Some Kinda Robot
-#BHLsrzYt-H4 0:52 Strong Bad Email #2 - Homsar
-#wRKrv-RlJOw 1:01 Strong Bad Email #6 - Depressio
-#Lml_AKkhCVY 1:01 Strong Bad Email #9 - I Love You
 `
