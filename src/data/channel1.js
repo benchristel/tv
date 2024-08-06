94f6e8fb5f3c9a3ba66a03b2e6e2ba1629f06166
Ben Christel <ben.christel@gmail.com>
2024-08-05 21:40:39 -0700

    Update videos

--- src/data/channel1.js
+++ src/data/channel1.js
@@ -3034,7 +3034,7 @@ M7BPlOirPig 17:18 Nikolai Rimsky-Korsakov | Russian Easter Overture op. 36
 kcq6DlSYwSo 2:16 Robert Führer | Praeludium über das böhmische Weihnachtslied „Geboren ist Christus, der Herr“
 4Wvhn__jwd8 3:58 Anonymus / Kerll | Toccata con il pedali
 2RQc1rVf-9Q 3:55 Domenico Zipoli | Pastorale
-yhp5aGqte5s 8:55 LIGETI | Sonata for cello solo - Narek Hakhnazaryan
+# yhp5aGqte5s 8:55 LIGETI | Sonata for cello solo - Narek Hakhnazaryan
 iX0Hf8FP09M 18:22 BACH | Suite No.1 in G-major BWV 1007 - Narek Hakhnazaryan
 ZQ8gGjFQEw4 10:06 MAHLER | Lieder
 TmGzdZGdtvw 11:17 MENDELSSOHN | Hebrides Overture, op. 26
@@ -4120,6 +4120,7 @@ IGzv4vufN-k 1:51:53 The Pirates Of Penzance (1983) full movie watch on YouTube H
 OVczrZAWc_4 2:10:03 Patience - Opera Australia
 vJGY6KIfwWA 2:17:28 The Mikado  2015 Gilbert & Sullivan
 x_E0zJU9hFY 2:18:45 The Mikado 2011 Gilbert&Sullivan
+f2TW90OEU-U 2:22:11 The Mikado 1992 Buxton
 7To1E2y5Aq0 2:29:28 The Gondoliers Stratford Festival Gilbert and Sullivan
 # lY7Y4asI4iQ 38:32 Trial By Jury, by Gilbert and Sullivan (2002 production)
 # BPooR7a_kNA 1:54:52 The Sorcerer, by Gilbert and Sullivan (2002 production)
