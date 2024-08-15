47580535684bf56fac2565f0efb5f31a404c0ff4
Ben Christel <ben.christel@gmail.com>
2024-08-14 20:19:49 -0700

    Convert Channel.js to TypeScript

--- src/Channel.js
+++
@@ -1,8 +0,0 @@
-// import type { Broadcast } from "./Broadcast"
-
-// FIXME
-// export interface Channel {
-//   getBroadcast(time: number): Broadcast;
-//   getName(): string;
-//   getTotalDuration(): number;
-// }
---
+++ src/Channel.ts
@@ -0,0 +1,7 @@
+import type { Broadcast } from "./Broadcast"
+
+export interface Channel {
+  getBroadcast(time: number): Broadcast;
+  getName(): string;
+  getTotalDuration(): number;
+}
