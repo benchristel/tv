b44b3b624561994eaaa5ad385734489eaef079f1
Ben Christel <ben.christel@gmail.com>
2024-08-14 20:18:15 -0700

    Convert Broadcast.js to TypeScript

---
+++ src/Broadcast.ts
@@ -0,0 +1,20 @@
+export type Broadcast =
+  | {
+      type: "nothing",
+      nextVideoId: string,
+      nextVideoStartTimestamp: number,
+    }
+  | {
+      type: "video",
+      videoId: string,
+      videoTitle: string,
+      currentTime: number,
+    }
+
+export function nothing(nextVideoId: string = "", nextVideoStartTimestamp: number = 0): Broadcast {
+  return {
+    type: "nothing",
+    nextVideoId,
+    nextVideoStartTimestamp
+  }
+}
