77a775c7ffdb6a6c4c5b8540ef78b558c7ad2081
Ben Christel <ben.christel@gmail.com>
2024-08-16 16:23:55 -0700

    Convert useNow to TypeScript

--- src/lib/useNow.ts
+++ src/lib/useNow.ts
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useInterval } from "./useInterval"
 
-export function useNow()/* FIXME : number */ {
+export function useNow(): number {
   const [now, setNow] = useState(+new Date())
   useInterval(() => setNow(+new Date()), 250)
   return now
