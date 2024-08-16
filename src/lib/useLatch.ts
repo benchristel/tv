f419c0fdfeff1531b86be741ebf8ee39e44f2713
Ben Christel <ben.christel@gmail.com>
2024-08-16 16:22:30 -0700

    Convert useLatch.js to TypeScript

--- src/lib/useLatch.ts
+++ src/lib/useLatch.ts
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export function useLatch()/* FIXME : [boolean, () => void] */ {
+export function useLatch(): [boolean, () => void] {
   const [v, set] = useState(false)
   return [v, () => set(true)]
 }
