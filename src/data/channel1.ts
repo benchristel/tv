eb07ecc9250d2816a013d4f10bdf220a1249e236
Ben Christel <ben.christel@gmail.com>
2025-09-03 21:05:37 -0700

    Add more Eddie Izzard

--- src/data/channel1.ts
+++ src/data/channel1.ts
@@ -3918,7 +3918,19 @@ TzjxFCqT-40 1:39:24 Sorrento, Italy Walking Tour in 4K
 nD3UlqgPN5U 22:12 Let’s make a Neolithic Longbow - Primitive Technology
 
 # Eddie Izzard
-QRB_GhLXCds 1:54:08 Eddie Izzard - Dress To Kill Show in1999.
+3zBjNYy-yqs 1:17:48 Wunderbar. 2022.
+DxQ0ny4Najc 1:25:24 Force Majeure 2013.
+rTtpJBkU_Ko 1:29:31 Eddie Izzard - Live From Wembley 2009.
+bSM7Uu7APoo 1:24:18 Eddie Izzard - Live at the Ambassadors 1993.
+OQaHSApf8-c 1:30:00 Eddie Izzard - Live at Madison Square Garden 2011
+NNvTflAiZgI 1:44:07 Stripped 2009.
+96JVxZd1YNw 1:36:38 Sexie 2003.
+tAG8_8Hwxoc 1:15:39 Unrepeatable 1994. Copyright and All rights reserved to AWAL Digital Limited (Kobalt).
+r3LU2Ilx1GE 1:41:40 Glorious 1997.
+FRfqHPW3Xlk 1:21:35 Eddie Izzard - Circle 2000.
+uihFwfZKtAI 55:50 Eddie Izzard - Dress To Circle, in Paris.
+QRB_GhLXCds 1:54:09 Eddie Izzard - Dress To Kill Show in1999.
+mDCGkasqgxQ 1:48:27 Eddie Izzard-Definite Article 1996.
 
 # Seinfeld
 2GO3X6RXIvs 1:09:32 Jerry Seinfeld on Broadway, I'm Telling You for the Last Time, FULL set, Stand-up Comedy, Live, 1998
